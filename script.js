cdb804508dec9ff4bd7c350c451da227bbb6340a
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-08-15 20:44:15 -0300

    UT2-TA11

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    let nums = [1, 2, 2, 3, 4, 4, 4, 5];
+    duplicates(nums);
+});
+
+
+/**
+ * Calcula la cantidad de elementos repetidos dentro del parámetro nums y lo imprime en consola
+ * @param {Array} nums - Un arreglo de números enteros.
+ */
+function duplicates(nums){
+    let duplicatedNumbers = [];
+    nums.forEach((num, index) => {
+        if(nums.indexOf(num, index + 1) !== -1 && !duplicatedNumbers.includes(num)){
+            duplicatedNumbers.push(num);
+        }
+    });
+    console.log(duplicatedNumbers.length);
+}
